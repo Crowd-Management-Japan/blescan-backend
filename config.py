24ab5c2321d8b8aba83e6d4173cde4a3d631368d
Blescan Hostinger <blescan@hostinger>
2024-02-06 00:38:21 +0000

    merged config matching for hostinger

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config:
     Class that contains static constants.
     """
     # hostname and port for the flask application
-    HOSTNAME = 'localhost'
-    PORT = 6002
+    HOSTNAME = '0.0.0.0'
+    PORT = 5000
 
     # server ip to deploy for raspberry installation
-    SERVER_IP = '127.0.0.1'
+    SERVER_IP = '194.195.92.252'
 
     OFFLINE_TIMEOUT = timedelta(seconds=30)
