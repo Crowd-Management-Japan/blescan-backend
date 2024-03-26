92acb22a944ae9704091e71907b151c46fbeae9b
Tobias Arens <tobias.arens@freenet.de>
2024-03-26 10:38:47 +0100

    change filename when saving logs

--- app/logging/routes.py
+++ app/logging/routes.py
@@ -3,26 +3,30 @@ import logging
 from config import Config
 import os
 from werkzeug.utils import secure_filename
+from datetime import datetime
 
 log_bp = Blueprint('log', __name__)
 
-@log_bp.route('upload_<int:id>/<string:filename>', methods=['POST'])
-def upload_logfile(id, filename):
-    logging.debug(f"file {filename} uploaded by {id}")
+@log_bp.route('upload_<int:id>', methods=['POST'])
+def upload_logfile(id):
+    logging.debug(f"file uploaded by {id}")
 
     folder = Config.RASPI_LOG_FOLDER
-
-    file = request.files.get(filename, None)
+    name = "log"
+    file = request.files.get(name, None)
 
     if not file:
         return "no file uploaded", 400
     else:
-
         filename = secure_filename(file.filename)
+        now = datetime.now()
+        filename = f"log_{now.year:02}{now.month:02}{now.day:02}_{now.hour:02}{now.minute:02}{now.second:02}.txt"
         folder = os.path.join(folder, f"{id}")
         if not os.path.exists(folder):
             os.makedirs(folder, exist_ok=True)
         file.save(os.path.join(folder, filename))
+
+        logging.debug(f"file saved as {filename}")
 
         return "ok"
     
