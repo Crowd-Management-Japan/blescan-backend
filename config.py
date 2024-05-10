1c904e2c21851529676a6676fb7db75bff3b4ae2
Blescan Hostinger <blescan@hostinger>
2024-05-10 01:05:35 +0000

    sensite info hidden just in case of issue when merging with git

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class Config:
     PORT = 5000
 
     # server ip to deploy for raspberry installation
-    SERVER_IP = '194.195.92.252'
+    SERVER_IP = '127.0.0.1'
 
     OFFLINE_TIMEOUT = timedelta(seconds=30)
