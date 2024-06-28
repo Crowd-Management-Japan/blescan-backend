0651e6af58dc28d60af62c4b35ce23aed99ca589
Blescan Hostinger <blescan@hostinger>
2024-06-28 02:00:33 +0000

    updated fields to reflect change in the algorithm on the client side.

--- app/database/models.py
+++ app/database/models.py
@@ -23,44 +23,50 @@ class User(db.Model, UserMixin):
 class CountEntry(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     timestamp: Mapped[datetime] = mapped_column(DateTime, primary_key=True)
+    scans: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
+    scantime: Mapped[float] = mapped_column(Float, nullable=True, default=None)
 
-    count: Mapped[int] = mapped_column(Integer)
-    close: Mapped[int] = mapped_column(Integer)
+    tot_all: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
+    tot_close: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
+    inst_all: Mapped[float] = mapped_column(Float, nullable=True, default=None)
+    inst_close: Mapped[float] = mapped_column(Float, nullable=True, default=None)
+    stat_all: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
+    stat_close: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
+
     rssi_avg: Mapped[float] = mapped_column(Float, nullable=True, default=None)
     rssi_std: Mapped[float] = mapped_column(Float, nullable=True, default=None)
     rssi_min: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
     rssi_max: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
 
+    rssi_thresh: Mapped[int] = mapped_column(Integer, nullable=True, default=None)
+    static_ratio: Mapped[float] = mapped_column(Float, nullable=True, default=None)
     latitude: Mapped[float] = mapped_column(Float, nullable=True, default=None)
     longitude: Mapped[float] = mapped_column(Float, nullable=True, default=None)
-
-    static_total: Mapped[int] = mapped_column(Integer)
-    static_close: Mapped[int] = mapped_column(Integer)
-
-    inst_all: Mapped[int] = mapped_column(Integer)
-    inst_close: Mapped[int] = mapped_column(Integer)
 
     @staticmethod
     def of_dict(data: Dict):
         entry = CountEntry()
         entry.id = data.get('id', None)
         entry.timestamp = data.get('timestamp', None)
+        entry.scans = data.get('scans', 0)
+        entry.scantime = data.get('scantime', 0)       
 
-        entry.count = data.get('count', 0)
-        entry.close = data.get('close', 0)
+        entry.tot_all = data.get('tot_all', 0)
+        entry.tot_close = data.get('tot_close', 0)
+        entry.inst_all = data.get('inst_all', 0)
+        entry.inst_close = data.get('inst_close', 0)
+        entry.stat_all = data.get('stat_all', 0)
+        entry.stat_close = data.get('stat_close', 0)
+
         entry.rssi_avg = data.get('rssi_avg', None)
         entry.rssi_std = data.get('rssi_std', None)
         entry.rssi_min = data.get('rssi_min', None)
         entry.rssi_max = data.get('rssi_max', None)
 
+        entry.rssi_thresh = data.get('rssi_thresh', None)
+        entry.static_ratio = data.get('static_ratio', None)
         entry.latitude = data.get('latitude', None)
         entry.longitude = data.get('longitude', None)
-
-        entry.static_total = data.get('static_total', 0)
-        entry.static_close = data.get('static_close', 0)
-
-        entry.inst_all = data.get('inst_all', 0)
-        entry.inst_close = data.get('inst_close', 0)
 
         return entry
 
@@ -68,17 +74,22 @@ class CountEntry(db.Model):
         data = {
             'id': self.id,
             'timestamp': self.timestamp.strftime(datetime_format),
-            'count': self.count,
-            'close': self.close,
+            'scans': self.scans,
+            'scantime': self.scantime,           
+            'tot_all': self.tot_all,
+            'tot_close': self.tot_close,
+            'inst_all':self.inst_all,
+            'inst_close':self.inst_close,
+            'stat_all':self.stat_all,
+            'stat_close':self.stat_close,
             'rssi_avg': self.rssi_avg,
             'rssi_std': self.rssi_std,
             'rssi_min': self.rssi_min,
             'rssi_max': self.rssi_max,
+            'rssi_thresh': self.rssi_thresh,
+            'static_ratio': self.static_ratio,           
             'latitude': self.latitude,
-            'longitude': self.longitude,
-            'static_total':self.static_total,
-            'static_close':self.static_close,
-            'inst_all':self.inst_all,
-            'inst_close':self.inst_close
+            'longitude': self.longitude
+
         }
         return data
